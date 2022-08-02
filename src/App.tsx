aa282117b221a3d8ce5c61e91666f36444e5d7bc
Parker Eudey <parkereudey@gmail.com>
2022-08-02 14:15:58 -0500

    moved graph to seperate component

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,8 @@
-import { useContext, useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
-import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts';
-import { search, getTickerData } from './utils/stockApi'; 
+import Graph from './components/graph'
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [desc, setDesc] = useState("");
-  const [symbol, setSymbol] = useState("");
-  const [tickerData, setTickerData] = useState<any[]>([]);
-
-  const getGridMarks = (data:any[]) => {
-    var grid:any[] = [];
-    var distance = (data.length/4)-1;
-    console.log(distance);
-    if(distance > 0){
-      grid.push(data[distance].name);
-      grid.push(data[distance*2].name);
-      grid.push(data[distance*3].name);
-    }
-    console.log(grid);
-  }
-
-  useEffect(() => {
-    var formattedData:any[] = [];
-    var json = search("AAPL");
-    json.then(data => {setDesc(data.result[0].description);
-      setSymbol(data.result[0].displaySymbol);
-    });
-    var json2 = getTickerData("AAPL");
-    json2.then(data => console.log(data));
-    json2.then(data => {
-      for(var i = 0; i < data.c?.length; i++){
-        formattedData.push({name: new Date(data?.t[i] * 1000).toDateString() , amt: data?.c[i]});
-      }
-    }).then(() => {
-      setTickerData(formattedData);
-    });
-
-  },[])
 
   return (
     <div className="App">
@@ -52,9 +16,6 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
@@ -62,14 +23,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-      <p>{symbol} || {desc}</p>
-      <LineChart width={600} height={600} margin={{ top: 5, right: 20, bottom: 50, left: 0 }} data={tickerData} >
-        <Line type="monotone" dataKey="amt" stroke="#8884d8" />
-        <CartesianGrid stroke="#ccc"/>
-        <XAxis dataKey="name" interval={7} angle={-35}/>
-        <YAxis />
-        <Tooltip />
-      </LineChart>
+      <Graph />
     </div>
   )
 }
---
+++ src/components/graph.tsx
@@ -0,0 +1,55 @@
+import { useEffect, useState } from 'react';
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts';
+import { search, getTickerData } from '../utils/stockApi';
+
+function Graph() {
+    const [count, setCount] = useState(0);
+    const [desc, setDesc] = useState("");
+    const [symbol, setSymbol] = useState("");
+    const [tickerData, setTickerData] = useState<any[]>([]);
+  
+    const getGridMarks = (data:any[]) => {
+      var grid:any[] = [];
+      var distance = (data.length/4)-1;
+      console.log(distance);
+      if(distance > 0){
+        grid.push(data[distance].name);
+        grid.push(data[distance*2].name);
+        grid.push(data[distance*3].name);
+      }
+      console.log(grid);
+    }
+  
+    useEffect(() => {
+      var formattedData:any[] = [];
+      var json = search("AAPL");
+      json.then(data => {setDesc(data.result[0].description);
+        setSymbol(data.result[0].displaySymbol);
+      });
+      var json2 = getTickerData("AAPL");
+      json2.then(data => console.log(data));
+      json2.then(data => {
+        for(var i = 0; i < data.c?.length; i++){
+          formattedData.push({name: new Date(data?.t[i] * 1000).toDateString() , amt: data?.c[i]});
+        }
+      }).then(() => {
+        setTickerData(formattedData);
+      });
+  
+    },[])
+
+    return (
+        <div>
+            <p>{symbol} || {desc}</p>
+            <LineChart width={600} height={600} margin={{ top: 5, right: 20, bottom: 50, left: 0 }} data={tickerData} >
+                <Line type="monotone" dataKey="amt" stroke="#8884d8" />
+                <CartesianGrid stroke="#ccc"/>
+                <XAxis dataKey="name" interval={7} angle={-35}/>
+                <YAxis />
+                <Tooltip />
+            </LineChart>
+        </div>
+    )
+}
+
+export default Graph;
