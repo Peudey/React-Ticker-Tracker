d6ff114ecac6ed1f2c03f4307515240126408350
Parker Eudey <parkereudey@gmail.com>
2022-08-06 20:57:45 -0500

    added timeframe filtering

--- src/components/graph.tsx
+++ src/components/graph.tsx
@@ -4,55 +4,53 @@ import { search, getTickerData } from '../utils/stockApi';
 import Search from "./searchBar"
 
 function Graph() {
-    const [count, setCount] = useState(0);
-    const [desc, setDesc] = useState("");
-    const [symbol, setSymbol] = useState("");
+    const [desc, setDesc] = useState("ALPHABET INC-CL A");
+    const [symbol, setSymbol] = useState("GOOGL");
     const [tickerData, setTickerData] = useState<any[]>([]);
     const [searchText, setSearchText] = useState("googl");
-    const [resolution, setResolution] = useState("D");
-  
-    const getGridMarks = (data:any[]) => {
-      var grid:any[] = [];
-      var distance = (data.length/4)-1;
-      console.log(distance);
-      if(distance > 0){
-        grid.push(data[distance].name);
-        grid.push(data[distance*2].name);
-        grid.push(data[distance*3].name);
-      }
-      console.log(grid);
-    }
+    const [timeframe, setTimeframe] = useState(30);
   
     useEffect(() => {
-      handleSearch();
-    },[])
+      refreshChart();
+    },[timeframe, symbol])
 
     const handleSearch = () => {
       var json = search(searchText);
-      json.then(data => {setDesc(data.result[0].description);
+      json.then(data => {
+        setDesc(data.result[0].description);
         setSymbol(data.result[0].displaySymbol);
       });
-      refreshChart();
-      return false;
     }
 
     const refreshChart = async() => {
       var formattedData:any[] = [];
-      var json = await getTickerData(searchText);
+      var json = await getTickerData(searchText, timeframe);
       for(var i = 0; i < json.c?.length; i++){
-        formattedData.push({date: new Date(json?.t[i] * 1000).toLocaleDateString('en-US', {timeZone:"Europe/London"}) , Value: json?.c[i].toFixed(2)});
+        formattedData.push({
+          date: new Date(json?.t[i] * 1000).toLocaleDateString('en-US', {timeZone:"Europe/London"}),
+          Value: json?.c[i].toFixed(2)
+        });
       }
       setTickerData(formattedData);
+    }
+
+    const handleFilter = (length: number) =>{
+      setTimeframe(length);
     }
 
     return (
         <div>
             <p>{symbol} ({desc})</p>
-            <Search 
-              searchText={searchText}
-              setSearchText={setSearchText}
-              handleSearch = {handleSearch}
-            />
+            <div>
+              <Search 
+                searchText={searchText}
+                setSearchText={setSearchText}
+                handleSearch = {handleSearch}
+              />
+              <button onClick={() => handleFilter(7)}>7</button>
+              <button onClick={() => handleFilter(31)}>M</button>
+              <button onClick={() => handleFilter(365)}>Y</button>
+            </div>
             <span className='chart'>
               <AreaChart width={1200} height={600} margin={{ top: 20, right: 20, bottom: 50, left: 0 }} data={tickerData} >
                 <defs>
@@ -63,7 +61,7 @@ function Graph() {
                 </defs>
                   <XAxis dataKey={'date'} interval={"preserveEnd"}/>
                   <YAxis />
-                  <Area type="monotone" dataKey="Value" stroke="#60afeb" fillOpacity={1} fill="url(#chartArea)" />
+                  <Area type="monotone" dataKey="Value" stroke="#7ebded" fillOpacity={1} fill="url(#chartArea)" />
                   <Tooltip labelStyle={{color: "black"}} itemStyle={{color: "black"}}/>
               </AreaChart>
             </span>
--- src/utils/stockApi.tsx
+++ src/utils/stockApi.tsx
@@ -7,12 +7,13 @@ export const search = async (symbol:string) => {
       throw new Error(message);
     }
   
-    console.log("test");
     return await response.json();
   }
 
-export const getTickerData = async(symbol:string) => {
-  const url = `https://finnhub.io/api/v1/stock/candle?symbol=${symbol}&resolution=D&from=${Math.floor(Date.now() / 1000)-2630000}&to=${Math.floor(Date.now() / 1000)}&token=cbcvu7qad3i1jffu0650`;
+export const getTickerData = async(symbol:string, length:number) => {
+  var newDate = new Date();
+  newDate.setDate(newDate.getDate() - length);
+  const url = `https://finnhub.io/api/v1/stock/candle?symbol=${symbol}&resolution=D&from=${Math.floor(newDate.getTime()/1000)}&to=${Math.floor(Date.now()/1000)}&token=cbcvu7qad3i1jffu0650`;
   const response = await fetch(url);
 
   if(!response.ok) {
