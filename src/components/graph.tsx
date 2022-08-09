e3c7d7dccc7d7a04e587c098d0b431ce6f6112e0
peudey <parkereudey@gmail.com>
2022-08-08 21:58:45 -0500

    button styling

--- src/components/graph.tsx
+++ src/components/graph.tsx
@@ -42,7 +42,6 @@ function Graph() {
 
     return (
         <div>
-            <p>{symbol} ({desc})</p>
             <div>
               <Search 
                 searchText = {searchText}
@@ -50,11 +49,16 @@ function Graph() {
                 handleSearch = {handleSearch}
               />
             </div>
-            <button onClick={() => handleFilter(7)}>7</button>
-            <button onClick={() => handleFilter(31)}>M</button>
-            <button onClick={() => handleFilter(365)}>Y</button>
             <span className='chart'>
-              <AreaChart width={1200} height={600} margin={{ top: 100, right: 20, bottom: 50, left: 0 }} data={tickerData} >
+            <div style={{textAlign:"left",  position: "relative", left: "3%"}}>
+              <p >{symbol} ({desc})</p>
+            </div>
+            <div className='chartButtons'>
+              <button onClick={() => handleFilter(7)}>7</button>
+              <button onClick={() => handleFilter(31)}>M</button>
+              <button onClick={() => handleFilter(365)}>Y</button>
+            </div>
+              <AreaChart width={1200} height={600} margin={{ top: 20, right: 20, bottom: 50, left: 0 }} data={tickerData} >
                 <defs>
                 <linearGradient id="chartArea" x1="0" y1="0" x2="0" y2="1">
                   <stop offset="5%" stopColor="#60afeb" stopOpacity={0.8}/>
